063a4e10829890d9db3b14d1baa732494a8d95b7
Mark Faust <markfaust123@gmail.com>
2023-06-22 12:13:54 -0400

    Initial Commit

---
+++ Dull/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dull
+//
+//  Created by Mark Faust on 6/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
